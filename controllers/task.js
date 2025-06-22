11db4b6c912688da8048efb105c620d6f663e2bf
tanwee14 <8409tan@gmail.com>
2025-06-22 19:00:00 +0530

    Continued task controller

--- controllers/task.js
+++ controllers/task.js
@@ -1,3 +1,11 @@
 import task from "../DB/models/task.js"
 import { createCustomError } from "../errors/custom-error.js";
 import asyncWrapper from "../middlewares/asyncWrapper.js";
+import asyncWrapper from "../middlewares/asyncWrapper.js";
+
+export const getAllTasks = asyncWrapper(
+    async (req, res) => {
+        const tasks = await task.find({});
+        res.status(200).json({ tasks });
+    }
+);
