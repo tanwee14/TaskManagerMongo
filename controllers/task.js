9f918f0439f93df904bf2657716fb8e5fb604a09
tanwee14 <8409tan@gmail.com>
2025-06-23 23:09:42 +0530

    Continued task controller

--- controllers/task.js
+++ controllers/task.js
@@ -1,11 +1,47 @@
 import task from "../DB/models/task.js"
 import { createCustomError } from "../errors/custom-error.js";
 import asyncWrapper from "../middlewares/asyncWrapper.js";
-import asyncWrapper from "../middlewares/asyncWrapper.js";
 
 export const getAllTasks = asyncWrapper(
     async (req, res) => {
         const tasks = await task.find({});
         res.status(200).json({ tasks });
+    }
+);
+
+export const getTask = asyncWrapper(
+    async (req, res, next) => {
+        const { id } = req.params;
+        const singleTask = await task.findById(id);
+
+        if (singleTask) {
+            return res.status(200).json({ singleTask });
+        }
+
+        const error = createCustomError('Task Not Found', 404);
+        next(error);
+    }
+);
+
+export const editTask = asyncWrapper(
+    async (req, res, next) => {
+
+        // get the task to be updated
+        const { id } = req.params;
+        const targetTask = await task.findById(id);
+
+        // check if the task not found
+        if (!targetTask) {
+            const error = createCustomError('Task Not Found', 404);
+            next(error);
+        }
+
+        // update the chosen one
+        const updatedTask = await task.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        return res.status(200).json({ message: `task updated successfully!`, updatedTask });
     }
 );
